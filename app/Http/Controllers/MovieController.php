acff82341c445e6e8f82219a32d211755719dade
Stefano <tepo@hotmail.it>
2021-05-13 16:22:37 +0200

    Creata la route e aggiunti metodi al controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function index() {
+        $movies = Movie::all();
+        return view("home", $movies);
+    }
 }
