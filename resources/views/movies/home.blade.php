0c1128338b738d34da90453c12cb24b679d03a9f
Stefano <tepo@hotmail.it>
2021-05-14 17:09:48 +0200

    Miglioramenti grafici (scss)

--- resources/views/movies/home.blade.php
+++ resources/views/movies/home.blade.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Home Movies</title>
   </head>
   <body>
     <h1>Lista movies</h1>
-    @foreach ($movies as $movie)
-      <h2>{{$movie->title}}</h2>
-      <a href="{{route("movies.show", ["movie" => $movie->id])}}">Dettagli film</a>
-    @endforeach
+    <div class="container">
+      @foreach ($movies as $movie)
+      <div class="box_movie">
+        <h3>{{$movie->title}}</h3>
+        <div class="link"><a href="{{route("movies.show", ["movie" => $movie->id])}}">Dettagli film</a></div>
+      </div>
+      @endforeach
+    </div>
   </body>
 </html>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Movies</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>{{$movie->title}} - Dettagli film</title>
   </head>
   <body>
-    <a href="{{route("movies.index")}}">Torna indietro</a>
-    <h2>{{$movie->title}}</h2>
-    <p>Regia: {{$movie->director}}</p>
-    <p>Durata: {{$movie->duration}} minuti</p>
-    <p>Generi: {{$movie->genre}}</p>
+    <div class="back"><a href="{{route("movies.index")}}">Torna indietro</a></div>
+    <div class="details">
+      <h1>{{$movie->title}}</h1>
+      <p>Regia: {{$movie->director}}</p>
+      <p>Durata: {{$movie->duration}} minuti</p>
+      <p>Generi: {{$movie->genre}}</p>
+    </div>
   </body>
 </html>
